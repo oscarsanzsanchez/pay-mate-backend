6d00f20aae2d019f0aa63fd36b643ae168ab5653
Oscar Sanz <sanzsanchezoscar@gmail.com>
2023-11-03 18:51:57 +0100

    feat: init Hapi server with hello world.
    Adds eslint and prettier

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import Hapi from "@hapi/hapi";
+
+const initServer = async () => {
+	const server = Hapi.server({
+		port: 3000,
+		host: "localhost"
+	});
+
+	server.route({
+		method: "GET",
+		path: "/",
+		handler: () => {
+			return "Hello World!";
+		}
+	});
+
+	await server.start();
+	console.log("Server running on %s 🚀", server.info.uri);
+};
+
+process.on("unhandledRejection", (err) => {
+	console.log(err);
+	process.exit(1);
+});
+
+initServer();
